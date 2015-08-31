842ae9e647c9ec846e311d8eaa82cd3ce70815d7
Sebastian Porto <sebasporto@gmail.com>
2015-08-31 22:05:44 +1000

    Added DevTools

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -5,11 +5,14 @@ import bows                              from 'bows';
 import React                             from 'react';
 import thunkMiddleware                   from 'redux-thunk';
 import loggerMiddleware                  from 'redux-logger';
+import { compose }                       from 'redux';
 import { applyMiddleware }               from 'redux';
 import { combineReducers }               from 'redux';
 import { createStore }                   from 'redux';
 import { Provider }                      from 'react-redux';
 import { Redirect, Router, Route }       from 'react-router';
+import { devTools, persistState }        from 'redux-devtools';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 import HashHistory                       from 'react-router/lib/HashHistory';
 import reduxCrud                         from 'redux-crud';
 import addMiddleware                     from './shared/services/addMiddleware';
@@ -24,13 +27,20 @@ import NotFound        from './shared/NotFound.jsx'
 import LanguagesList   from './languages/ListLoader.jsx'
 
 addMiddleware();
-const log = bows('app')
+const log           = bows('app')
 const history       = new HashHistory();
+
+const finalCreateStore = compose(
+  // Provides support for DevTools:
+  devTools(),
+  // Lets you write ?debug_session=<name> in address bar to persist debug sessions
+  createStore
+);
 
 const createStoreWithMiddleware = applyMiddleware(
 	thunkMiddleware, // lets us dispatch() functions
 	loggerMiddleware // neat middleware that logs actions
-)(createStore);
+)(finalCreateStore);
 
 const allReducers = combineReducers({
 	languages:       reduxCrud.reducersFor('languages'),
@@ -78,9 +88,14 @@ AppRouter.propTypes = {
 
 const mountNode = document.getElementById('app');
 React.render(
-	<Provider store={store}>
-		{() => <AppRouter history={history} /> }
-	</Provider>,
+	<div>
+		<Provider store={store}>
+			{() => <AppRouter history={history} /> }
+		</Provider>
+		<DebugPanel top right bottom>
+			<DevTools store={store} monitor={LogMonitor} />
+		</DebugPanel>
+	</div>,
 	mountNode
 );
 
