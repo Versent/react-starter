dc8fa18cfa617b597134c6cfe747a4cfdae271a4
Sebastian Porto <sebasporto@gmail.com>
2015-09-04 09:52:57 +1000

    Added missing commas to webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,9 +1,9 @@
-var path    = require('path');
-var webpack = require('webpack');
+var path    = require('path')
+var webpack = require('webpack')
 
 // https://github.com/webpack/extract-text-webpack-plugin
 // Used for extracting CSS into its own file
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 var webpackDevServerHost = 'webpack-dev-server/client?http://0.0.0.0:4002' // WebpackDevServer host and port
 var WebpackDevServerOnlyDev = 'webpack/hot/only-dev-server'
@@ -32,51 +32,51 @@ module.exports = {
     app:           [
       webpackDevServerHost,
       WebpackDevServerOnlyDev,
-      './src/app.jsx'
-    ]
+      './src/app.jsx',
+    ],
   },
   output: {
     path: path.join(__dirname, 'public', 'bundles'),
     filename:    '[name].js',
-    publicPath:  publicPath
+    publicPath:  publicPath,
   },
   module: {
     preLoaders: [{
       test:    /\.js|\.jsx$/,
       exclude: /node_modules/,
-      loader: 'jscs-loader'
-    }],
+      loader: 'jscs-loader',
+    },],
     loaders: [
       {
         test: /\.less$/,
-        loader: ExtractTextPlugin.extract('style', 'css!autoprefixer-loader?browsers=last 2 version!less')
+        loader: ExtractTextPlugin.extract('style', 'css!autoprefixer-loader?browsers=last 2 version!less'),
       },
       {
         test: /\.css/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
       },
       {
         test: /\.js|\.jsx/,
         include: path.join(__dirname, 'src'),
-        loaders: ['react-hot', 'babel?stage=1']
+        loaders: ['react-hot', 'babel?stage=1'],
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&minetype=application/font-woff'
+        loader: 'url-loader?limit=10000&minetype=application/font-woff',
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader'
-      }
-    ]
+        loader: 'file-loader',
+      },
+    ],
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new ExtractTextPlugin('bundle.css')
+    new ExtractTextPlugin('bundle.css'),
   ],
   node: { // mocks for Joi validation
     net: 'empty',
     tls: 'empty',
-    dns: 'empty'
-  }
-};
+    dns: 'empty',
+  },
+}
